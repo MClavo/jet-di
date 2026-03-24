0054016231cffa203dea111acf66cf810ac37318
MClavo <m.clavo.conesa@gmail.com>
2026-03-24 17:31:48 +0100

    feat: add annotations for target and retention policies in Fuel qualifier

--- jet-core/src/main/java/org/mclavo/annotation/Fuel.java
+++ jet-core/src/main/java/org/mclavo/annotation/Fuel.java
@@ -1,8 +1,18 @@
 package org.mclavo.annotation;
 
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
 /**
  * Declares a qualifier value for a bean provided by a {@code @Part} method.
  */
+@Target({
+    ElementType.METHOD,
+    ElementType.PARAMETER
+})
+@Retention(RetentionPolicy.RUNTIME)
 public @interface Fuel {
     /**
      * @return qualifier name associated with the produced bean
